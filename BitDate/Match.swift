6e5c4608c808b2263812e8318cd92712f7fc6f22
Piri-Piri <david.pirih@gmx.de>
2015-04-23 08:04:27 +0200

    bugfix?: replace orderByAscending with reserve(users)

--- BitDate/Match.swift
+++ BitDate/Match.swift
@@ -20,19 +20,22 @@ func fetchMatches(callBack: ([Match]) -> ()) {
         .findObjectsInBackgroundWithBlock({
         objects, error in
             if let matches = objects as? [PFObject] {
+                
                 let matchedUser = matches.map({
                     (object)->(matchID: String, userID: String)
                     in
                     (object.objectId! as String, object.objectForKey("toUser") as! String)
                 })
                 let userIDs = matchedUser.map({$0.userID})
+                
                 PFUser.query()?
                     .whereKey("objectId", containedIn: userIDs)
-                    .orderByAscending("objectId")
+                    //.orderByAscending("objectId")
                     .findObjectsInBackgroundWithBlock({
                         objects, error in
                         if let users = objects as? [PFUser] {
-                            //var users = reverse(users)
+                            var users = reverse(users)
+                            println(users)
                             var m: [Match] = []
                             for (index, user) in enumerate(users) {
                                 m.append(Match(id: matchedUser[index].matchID, user: pfUserToUser(user)))
